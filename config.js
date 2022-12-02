7f4a97be3c0ea6e3e50a20bc819170f44fbd5c60
EPBr3uby <117103683+EPBr3uby@users.noreply.github.com>
2022-12-02 11:36:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
+global.Owner = ['2347031350146']
+global.OwnerNumber = ['2347031350146']
+global.ownertag = ['2347031350146']
+global.BotName = "REUBY"
+global.packname = "DR REUBY"
+global.author = "By: REUBEN"
+global.OwnerName = "REUBY"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
